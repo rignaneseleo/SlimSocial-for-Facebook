dd07ad3763dc15de8703b2121581969bcb6d0a87
leorigna <rignanese.leo@outlook.com>
2015-11-21 17:05:35 +0100

    First Commit

---
+++ SlimFacebook/app/build/generated/source/buildConfig/debug/it/rignanese/leo/slimfacebook/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package it.rignanese.leo.slimfacebook;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "it.rignanese.leo.slimfacebook";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "1.1";
+}
