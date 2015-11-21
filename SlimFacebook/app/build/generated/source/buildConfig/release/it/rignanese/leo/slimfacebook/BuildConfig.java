c772097cf5b283f8ba215ea4bbd88a7a9b8af715
leorigna <rignanese.leo@outlook.com>
2015-11-21 21:31:31 +0100

    version 1.2

---
+++ SlimFacebook/app/build/generated/source/buildConfig/release/it/rignanese/leo/slimfacebook/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package it.rignanese.leo.slimfacebook;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "it.rignanese.leo.slimfacebook";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 3;
+  public static final String VERSION_NAME = "1.2";
+}
