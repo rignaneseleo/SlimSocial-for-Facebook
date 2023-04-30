dd2f94009635a3ff41673900e53058e4503dba34
Leo <dev.rignaneseleo@gmail.com>
2023-04-30 15:01:41 -0600

    optimized imports

--- SlimFacebook/app/src/test/java/it/rignanese/leo/slimfacebook/ExampleUnitTest.java
+++ SlimFacebook/app/src/test/java/it/rignanese/leo/slimfacebook/ExampleUnitTest.java
@@ -1,8 +1,8 @@
 package it.rignanese.leo.slimfacebook;
 
-import org.junit.Test;
-
 import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
