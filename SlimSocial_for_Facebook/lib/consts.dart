0a12e2d82e290fb6d41c552f83f5aa75daff1de0
Leo <dev.rignaneseleo@gmail.com>
2023-05-05 14:36:18 -0600

    app completely rebuild in flutter

---
+++ SlimSocial_for_Facebook/lib/consts.dart
@@ -0,0 +1,29 @@
+const String kFacebookHomeUrl = 'https://touch.facebook.com/home.php';
+const String kFacebookHomeBasicUrl = 'https://mbasic.facebook.com/home.php';
+const String kMessengerUrl = 'https://www.messenger.com';
+
+const List<String> kPermittedHostnames = [
+  "facebook.com",
+  "fbcdn.net",
+  "fb.com",
+  "fb.me",
+  "messenger.com",
+];
+
+//suffix for the feed
+const String suffixRecentFirst = "?sk=h_chr";
+const String suffixDefault = "?sk=h_nor";
+
+//user agent for the webview
+const String kFirefoxUserAgent =
+    "Mozilla/5.0 (X11; CrOS x86_64 10066.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36";
+const String kOperaMiniUserAgent =
+    "Opera/9.80 (Android; Opera Mini/69.0.2254/191.303; U; en) Presto/2.12.423 Version/12.16";
+
+
+
+const String kEmailToDevUrl="mailto:dev.rignaneseleo+slimsocial@gmail.com?subject=SlimSocial%20for%20Facebook%20feedback";
+const String kDevEmail="dev.rignaneseleo+slimsocial@gmail.com";
+const String kTwitterProfileUrl="https://twitter.com/leorigna";
+const String kGithubProjectUrl="https://github.com/rignaneseleo/SlimSocial-for-Facebook";
+const String kPlayStoreUrl="https://play.google.com/store/apps/details?id=it.rignanese.leo.slimfacebook";
---
+++ SlimSocial_for_Facebook/lib/utils.dart
@@ -0,0 +1,64 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_custom_tabs/flutter_custom_tabs.dart';
+import 'package:flutter_file_downloader/flutter_file_downloader.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:slimsocial_for_facebook/style/color_schemes.g.dart';
+
+void launchInAppUrl(BuildContext context, String url) async {
+  try {
+    await launch(
+      url,
+      customTabsOption: CustomTabsOption(
+        toolbarColor: Theme.of(context).primaryColor,
+        enableDefaultShare: true,
+        enableUrlBarHiding: false,
+        showPageTitle: true,
+        enableInstantApps: true,
+        extraCustomTabs: const <String>[
+          // ref. https://play.google.com/store/apps/details?id=org.mozilla.firefox
+          'org.mozilla.firefox',
+          // ref. https://play.google.com/store/apps/details?id=com.microsoft.emmx
+          'com.microsoft.emmx',
+        ],
+      ),
+      safariVCOption: SafariViewControllerOption(
+        preferredBarTintColor: Theme.of(context).primaryColor,
+        preferredControlTintColor: Colors.white,
+        barCollapsingEnabled: true,
+        entersReaderIfAvailable: false,
+        dismissButtonStyle: SafariViewControllerDismissButtonStyle.close,
+      ),
+    );
+  } catch (e) {
+    // An exception is thrown if browser app is not installed on Android device.
+    debugPrint(e.toString());
+  }
+}
+
+void showToast(String text) => Fluttertoast.showToast(
+      msg: text,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 1,
+      backgroundColor: FacebookColors.blue,
+      textColor: FacebookColors.white,
+      fontSize: 16.0,
+    );
+
+extension StringExtension on String {
+  String capitalize() {
+    return "${this[0].toUpperCase()}${this.substring(1).toLowerCase()}";
+  }
+}
+
+Future<String?> downloadImage(String url) async {
+  var file = await FileDownloader.downloadFile(
+    url: url,
+    onDownloadError: (String? error) {
+      showToast("error_trylater".tr());
+    },
+  );
+  return file?.path;
+}
