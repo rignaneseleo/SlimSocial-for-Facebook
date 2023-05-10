907054b84889fbc4af7be1677d5b4652bee65ed9
Leo <dev.rignaneseleo@gmail.com>
2023-05-10 11:40:09 -0600

    remove ads optimized

--- SlimSocial_for_Facebook/lib/utils/js.dart
+++ SlimSocial_for_Facebook/lib/utils/js.dart
@@ -2,7 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 
 class CustomJs {
   static String editCss(String css) {
-    var str= '''
+    var str = '''
       javascript:function addStyle(css) {
         var node = document.createElement('style');
         node.innerHTML = css;
@@ -18,75 +18,99 @@ class CustomJs {
 		}
 		foo();""";
 
-  static String removeAds = """javascript:function removeAds() {
-	      var adKeywords = [
-            // French
-            "Sponsorisé",
-            // English
-            "Sponsored",
-            // Spanish
-            "Patrocinado",
-            "Publicidad",
-            // German
-            "Gesponsert",
-            // Italian
-            "Sponsorizzato",
-            // Swedish
-            "Sponsrad",
-            // Vietnamese
-            "Được tài trợ",
-            // Chinese (Traditional and Simplified)
-            "贊助內容",
-            "赞助内容",
-            // Japanese
-            "スポンサーされた投稿",
-            // Polish
-            "Sponsorowane",
-            // Russian
-            "Реклама",
-            // Croatian
-            "Sponzorirano",
-            // Hindi
-            "प्रायोजित",
-            // Bengali
-            "স্পনসরড",
-            // Tamil
-            "பராமரிக்கப்பட்ட",
-            // Telugu
-            "ప్రచారం చేసిన",
-            // Kannada
-            "ಪ್ರಾಯೋಜಿತ",
-            // Malayalam
-            "സ്പോൺസർ ചെയ്യപ്പെട്ട",
-            // Punjabi
-            "ਸਰਪ੍ਰਸਤ",
-            // Marathi
-            "प्रायोजित",
-            // Gujarati
-            "સ્પોન્સર્ડ",
-            // Urdu
-            "سپانسرڈ",
-            // Thai
-            "โพสต์ที่ได้รับการสนับสนุน",
-            "${"sponsored_keyword_fb".tr()}"
-        ];
-        
-        var myDiv = '<div class="_52z5" style="z-index: 0; height: 135px; display: flex; justify-content: center; align-items: center;"> <div style="text-align: center;"><h1 style="color: white; font-size: 35px; height: 28px; margin: 0;">${"ad_removed".tr()}</h1><p style="color: white; font-size: 12px; margin: 0;">${"Thanks to SlimSocial".tr()}</p></div></div>';
-        
-        var spans = document.getElementsByTagName("span");
-        
-        var adsCount = 0;
-        for (var i = 0; i < spans.length; i++) {
-            if (adKeywords.includes(spans[i].textContent)) {
-                var found = spans[i];
-                var post = found.closest("article");
-                post.innerHTML = (myDiv);
-                adsCount++;
-            }
-        }
-        (adsCount + " ads replaced");
-		}
-		removeAds();""";
+  static String removeAds = """
+  javascript: function removeAds() {
+  var adKeywords = [
+    // French
+    "Sponsorisé",
+    // English
+    "Sponsored",
+    // Spanish
+    "Patrocinado",
+    "Publicidad",
+    // German
+    "Gesponsert",
+    // Italian
+    "Sponsorizzato",
+    // Swedish
+    "Sponsrad",
+    // Vietnamese
+    "Được tài trợ",
+    // Chinese (Traditional and Simplified)
+    "贊助內容",
+    "赞助内容",
+    // Japanese
+    "スポンサーされた投稿",
+    // Polish
+    "Sponsorowane",
+    // Russian
+    "Реклама",
+    // Croatian
+    "Sponzorirano",
+    // Hindi
+    "प्रायोजित",
+    // Bengali
+    "স্পনসরড",
+    // Tamil
+    "பராமரிக்கப்பட்ட",
+    // Telugu
+    "ప్రచారం చేసిన",
+    // Kannada
+    "ಪ್ರಾಯೋಜಿತ",
+    // Malayalam
+    "സ്പോൺസർ ചെയ്യപ്പെട്ട",
+    // Punjabi
+    "ਸਰਪ੍ਰਸਤ",
+    // Marathi
+    "प्रायोजित",
+    // Gujarati
+    "સ્પોન્સર્ડ",
+    // Urdu
+    "سپانسرڈ",
+    // Thai
+    "โพสต์ที่ได้รับการสนับสนุน",
+    "${"sponsored_keyword_fb".tr()}",
+  ];
+
+  var myDiv = '<div class="_52z5" style="z-index: 0; height: 135px; display: flex; justify-content: center; align-items: center;"> <div style="text-align: center;"><h1 style="color: white; font-size: 35px; height: 28px; margin: 0;">${"ad_removed".tr()}</h1><p style="color: white; font-size: 12px; margin: 0;">${"Thanks to SlimSocial".tr()}</p></div></div>';
+
+  var spans = document.getElementsByTagName("span");
+
+  const adSpans = [...document.querySelectorAll('span')].filter(span =>
+    adKeywords.some(keyword => span.textContent.includes(keyword))
+  );
+  let adsCount = 0;
+  for (const span of adSpans) {
+    const post = span.closest("article");
+    post.innerHTML = myDiv;
+    adsCount++;
+  }
+
+  console.log(adsCount + ` ads removed`);
+  }
+
+// Select the node that will be observed for changes
+const targetNode = document.body;
+
+// Create a new observer object
+const observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    // Filter out added nodes that are not <section> elements
+    const addedSections = Array.from(mutation.addedNodes).filter(node => node.nodeName === 'SECTION');
+    
+    // Check if any new <section> elements were added
+    if (addedSections.length) {
+       removeAds();
+    }
+  });
+});
+
+// Options for the observer (which mutations to observe)
+const config = { childList: true, subtree: true };
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
+  """;
 }
 
 String createFabFunc = """javascript:function createFab() {
